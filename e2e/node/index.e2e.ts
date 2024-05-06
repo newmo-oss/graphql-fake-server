a1a1e3536c23dc7fe6c8e6dd21914d16a2313ef1
azu <azuciao@gmail.com>
2024-05-06 17:00:01 +0900

    test: fix test

--- e2e/node/index.e2e.ts
+++ e2e/node/index.e2e.ts
@@ -7,7 +7,7 @@ describe("integration test", async () => {
         const server = await createFakeServer({
             schemaFilePath: "./1-basic-schema.graphql",
         });
-        closeServer = server.stop;
+        server = server.stop;
     });
     afterAll(() => {
         closeServer?.();
