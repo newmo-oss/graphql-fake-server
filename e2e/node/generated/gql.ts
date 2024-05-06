97abec7ced0c4b6dea656f60b0eea90822f0d78e
azu <azuciao@gmail.com>
2024-05-07 00:38:19 +0900

    fix use 127.0.0.1

--- e2e/node/generated/gql.ts
+++ e2e/node/generated/gql.ts
@@ -1,6 +1,6 @@
-import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/core";
 /* eslint-disable */
-import * as types from "./graphql";
+import * as types from './graphql';
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
 /**
  * Map of all GraphQL operations in the project.
@@ -13,8 +13,7 @@ import * as types from "./graphql";
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
-    "query GetBooks {\n  books {\n    id\n    title\n  }\n}\n\nmutation CreateBook($title: String!) {\n  createBook(input: {title: $title}) {\n    id\n    title\n  }\n}":
-        types.GetBooksDocument,
+    "query GetBooks {\n  books {\n    id\n    title\n  }\n}\n\nmutation CreateBook($title: String!) {\n  createBook(input: {title: $title}) {\n    id\n    title\n  }\n}": types.GetBooksDocument,
 };
 
 /**
@@ -34,13 +33,10 @@ export function graphql(source: string): unknown;
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(
-    source: "query GetBooks {\n  books {\n    id\n    title\n  }\n}\n\nmutation CreateBook($title: String!) {\n  createBook(input: {title: $title}) {\n    id\n    title\n  }\n}",
-): (typeof documents)["query GetBooks {\n  books {\n    id\n    title\n  }\n}\n\nmutation CreateBook($title: String!) {\n  createBook(input: {title: $title}) {\n    id\n    title\n  }\n}"];
+export function graphql(source: "query GetBooks {\n  books {\n    id\n    title\n  }\n}\n\nmutation CreateBook($title: String!) {\n  createBook(input: {title: $title}) {\n    id\n    title\n  }\n}"): (typeof documents)["query GetBooks {\n  books {\n    id\n    title\n  }\n}\n\nmutation CreateBook($title: String!) {\n  createBook(input: {title: $title}) {\n    id\n    title\n  }\n}"];
 
 export function graphql(source: string) {
-    return (documents as any)[source] ?? {};
+  return (documents as any)[source] ?? {};
 }
 
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> =
-    TDocumentNode extends DocumentNode<infer TType, any> ? TType : never;
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
