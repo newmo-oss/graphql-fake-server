666db7bae9001e838db6c28acf7fb2869cb378bd
azu <azuciao@gmail.com>
2024-05-06 19:28:05 +0900

    feat: implement fake server

--- packages/@newmo/graphql-fake-server/src/server.test.ts
+++ packages/@newmo/graphql-fake-server/src/server.test.ts
@@ -12,7 +12,10 @@ const getPorts = () => {
         apolloServer: 4001 + portCounter,
     };
 };
-const startTestFakeServer = async ({ schemaString,ports }: { schemaString: string;ports: ReturnType<typeof getPorts> }) => {
+const startTestFakeServer = async ({
+    schemaString,
+    ports,
+}: { schemaString: string; ports: ReturnType<typeof getPorts> }) => {
     const schema = buildSchema(extendSchema(schemaString));
     const mockObject = await createMock({
         schema,
@@ -26,6 +29,7 @@ const startTestFakeServer = async ({ schemaString,ports }: { schemaString: strin
         ports: ports,
         maxDepth: 3,
         maxFieldRecursionDepth: 4,
+        maxRegisteredSequences: 100,
     });
 };
 describe("graphql-fake-server", () => {
@@ -51,12 +55,13 @@ describe("graphql-fake-server", () => {
     }
 `;
         const ports = getPorts();
-        const server = await startTestFakeServer({ schemaString: schema,ports });
+        const server = await startTestFakeServer({ schemaString: schema, ports });
         await server.start();
         const response = await fetch(`http://localhost:${ports.fakeServer}/graphql`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
+                "sequence-id": "test-sequence-id",
             },
             body: JSON.stringify({
                 query: `
@@ -148,6 +153,102 @@ describe("graphql-fake-server", () => {
                 },
               ],
             },
+          }
+        `);
+    });
+    it("should return registered fake response", async () => {
+        const schema = `
+    enum BookGenre {
+        FICTION
+        NON_FICTION
+    }
+    type Book {
+        id: ID! @exampleID(value: "book-id")
+        title: String! @exampleString(value: "The Great Gatsby")
+        genre: BookGenre! @exampleString(value: "FICTION")
+    }
+    type Author {
+        id: ID! @exampleID(value: "author-id")
+        name: String! @exampleString(value: "F. Scott Fitzgerald")
+        age: Int! @exampleInt(value: 33)
+        books: [Book!]!
+    }
+    type Query {
+        authors: [Author!]!
+    }
+`;
+        const ports = getPorts();
+        const server = await startTestFakeServer({ schemaString: schema, ports });
+        await server.start();
+        // register seed
+        await fetch(`http://localhost:${ports.fakeServer}/register-operation`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "sequence-id": "test-sequence-id",
+            },
+            body: JSON.stringify({
+                operationName: "authors",
+                type: "operation",
+                data: {
+                    authors: [
+                        {
+                            id: "override-author-id",
+                            name: "F. Scott Fitzgerald",
+                            age: 33,
+                            books: [
+                                {
+                                    id: "book-id1",
+                                    title: "The Great Gatsby",
+                                    genre: "FICTION",
+                                },
+                            ],
+                        },
+                    ],
+                },
+            }),
+        });
+        //request with sequence-id
+        const response = await fetch(`http://localhost:${ports.fakeServer}/graphql`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "sequence-id": "test-sequence-id",
+            },
+            body: JSON.stringify({
+                query: `
+                    query {
+                      authors {
+                        id
+                        name
+                        age
+                        books {
+                          id
+                          title
+                          genre
+                        }
+                      }
+                    }
+                `,
+            }),
+        });
+        const result = await response.json();
+        expect(result).toMatchInlineSnapshot(`
+          {
+            "authors": [
+              {
+                "age": 33,
+                "books": [
+                  {
+                    "genre": "FICTION",
+                    "id": "book-id1",
+                    "title": "The Great Gatsby",
+                  },
+                ],
+                "id": "override-author-id",
+                "name": "F. Scott Fitzgerald",
+              },
+            ],
           }
         `);
     });
--- packages/@newmo/graphql-fake-server/src/server.ts
+++ packages/@newmo/graphql-fake-server/src/server.ts
@@ -4,7 +4,7 @@ import { addMocksToSchema } from "@graphql-tools/mock";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 //@ts-expect-error
 import depthLimit from "graphql-depth-limit";
-import type { LogLevel } from "./logger.js";
+import { createLogger, LogLevel } from "./logger.js";
 import fs from "node:fs/promises";
 import { buildSchema } from "graphql/utilities/index.js";
 import { createMock, type MockObject } from "./createMock.js";
@@ -28,6 +28,12 @@ export type CreateFakeServerOptions = {
      * Default is maxDepth + 1
      */
     maxFieldRecursionDepth?: number;
+    /**
+     * max number of registered sequences
+     * Default is 100
+     * If the number of registered sequences exceeds this number, the oldest sequence is deleted.
+     */
+    maxRegisteredSequences?: number;
     logLevel?: LogLevel;
 };
 
@@ -40,6 +46,7 @@ type FakeServerInternal = {
     };
     maxDepth: number;
     maxFieldRecursionDepth: number;
+    maxRegisteredSequences: number;
     logLevel: LogLevel;
 };
 const creteApolloServer = async (options: FakeServerInternal) => {
@@ -59,21 +66,68 @@ const creteApolloServer = async (options: FakeServerInternal) => {
     });
     return server;
 };
+type SequenceRegistration =
+    | {
+          type: "network-error";
+          statusCode: number;
+          errors: Record<string, unknown>[];
+      }
+    | {
+          type: "operation";
+          data: Record<string, unknown>;
+      };
+// TODO: more strict validation?
+const validateSequenceRegistration = (data: unknown): data is SequenceRegistration => {
+    if (typeof data !== "object" || data === null) return false;
+    if ("type" in data && typeof data.type === "string") {
+        if (data.type === "network-error") {
+            return (
+                "errors" in data &&
+                Array.isArray(data.errors) &&
+                "statusCode" in data &&
+                typeof data.statusCode === "number"
+            );
+        }
+        if (data.type === "operation") {
+            return "data" in data && typeof data.data === "object";
+        }
+    }
+    return false;
+};
+class LRUMap<K, V> {
+    private map = new Map<K, V>();
+    private keys: K[] = [];
+    private maxSize: number;
+    constructor({ maxSize }: { maxSize: number }) {
+        this.maxSize = maxSize;
+    }
+    set(key: K, value: V) {
+        this.map.set(key, value);
+        this.keys.push(key);
+        if (this.keys.length > this.maxSize) {
+            const oldestKey = this.keys.shift();
+            if (oldestKey) {
+                this.map.delete(oldestKey);
+            }
+        }
+    }
+    get(key: K): V | undefined {
+        return this.map.get(key);
+    }
+}
 const createRoutingServer = async ({
-    apollo,
+    logLevel,
     ports,
+    maxRegisteredSequences,
 }: {
-    apollo: ApolloServer;
+    logLevel: LogLevel;
+    maxRegisteredSequences: number;
     ports: {
         fakeServer: number;
         apolloServer: number;
     };
 }) => {
-    const app = new Hono();
-    // app.post("/register-operation", async (req, res) => {});
-    app.use("/graphql", async (c) => {
-        return passToApollo(c);
-    });
+    const logger = createLogger(logLevel);
     // pass through to apollo server
     const passToApollo = async (c: Context) => {
         // remove prefix
@@ -92,10 +146,86 @@ const createRoutingServer = async ({
             body: c.req.raw.body,
             duplex: "half",
         });
+        // log response with pipe
         if (rep.status === 101) return rep;
         return new Response(rep.body, rep);
     };
-    app.all("/*", passToApollo);
+    const sequenceLruMap = new LRUMap<string, SequenceRegistration>({
+        maxSize: maxRegisteredSequences,
+    });
+    const app = new Hono();
+    app.post("/register-operation", async (c) => {
+        const sequenceId = c.req.header("sequence-id");
+        if (!sequenceId) {
+            c.status(400);
+            c.body("sequence-id is required");
+            return;
+        }
+        const body = await c.req.json();
+        if (!validateSequenceRegistration(body)) {
+            c.status(400);
+            c.body("invalid body");
+            return;
+        }
+        logger.debug(`register-operation: ${sequenceId}`, {
+            type: body.type,
+        });
+        sequenceLruMap.set(sequenceId, body);
+        c.status(200);
+        c.body({
+            ok: true,
+            sequenceId,
+        });
+    });
+    app.use("/graphql", async (c) => {
+        /**
+         * Steps:
+         * 1. Receive a request for a GraphQL query
+         * 2. Does it contain a sequence id?
+         *    - if Yes: type is network error → return an error
+         *    - if No: Pass through to Apollo Server -> exit
+         * 3. Send a request to Apollo Server
+         * 4. Merge the registration data with the response from 3
+         * 5. Return the merged data
+         */
+        const sequenceId = c.req.header("sequence-id");
+        // 2. Does it contain a sequence id?
+        if (!sequenceId) return passToApollo(c);
+        const sequence = sequenceLruMap.get(sequenceId);
+        logger.debug(`request sequence-id: ${sequenceId}`, {
+            sequence,
+        });
+        if (!sequence) return passToApollo(c);
+        if (sequence.type === "network-error") {
+            return new Response(JSON.stringify(sequence.errors), {
+                status: sequence.statusCode,
+            });
+        }
+        // 3. Send a request to Apollo Server
+        logger.debug(`request to apollo-server: ${sequenceId}`);
+        const rep = await fetch(`http://localhost:${ports.apolloServer}/graphql`, {
+            method: c.req.method,
+            headers: c.req.raw.headers,
+            body: c.req.raw.body,
+            duplex: "half",
+        });
+        if (rep.status === 101) return rep;
+        // 4. Does the request contain a sequence id?
+        const responseBody = await rep.json();
+        // 5. Merge the registration data with the response from 2
+        const data = sequence.data;
+        logger.debug(`merge sequence-id: ${sequenceId}`, {
+            data,
+            responseBody,
+        });
+        const merged = {
+            //@ts-expect-error
+            ...responseBody.data,
+            ...data,
+        };
+        return Response.json(merged, rep);
+    });
+
     return app;
 };
 export const createFakeServer = async (options: CreateFakeServerOptions) => {
@@ -111,12 +241,14 @@ export const createFakeServer = async (options: CreateFakeServerOptions) => {
     };
     const maxDepth = options.maxDepth ?? 3;
     const maxFieldRecursionDepth = options.maxFieldRecursionDepth ?? maxDepth + 1;
+    const maxRegisteredSequences = options.maxRegisteredSequences ?? 100;
     return createFakeServerInternal({
         ports,
         schema,
         mockObject,
         maxDepth,
         maxFieldRecursionDepth,
+        maxRegisteredSequences,
         logLevel: options.logLevel ?? "info",
     });
 };
@@ -126,6 +258,7 @@ export const createFakeServerInternal = async (options: FakeServerInternal) => {
     const routingServer = await createRoutingServer({
         apollo: apolloServer,
         ports: options.ports,
+        maxRegisteredSequences: options.maxRegisteredSequences,
     });
     let routerServer: ReturnType<typeof serve> | null = null;
     return {
