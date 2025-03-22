bea7f948ef7ec45885ba630c791267a660d6bff0
azu <azu@users.noreply.github.com>
2025-03-22 18:10:35 +0900

    feat: add vitest configuration files and update package dependencies (#64)

---
+++ packages/@newmo/graphql-fake-server/vitest.config.ts
@@ -0,0 +1,11 @@
+import GithubActionsReporter from "vitest-github-actions-reporter";
+import { defineConfig } from "vitest/config";
+
+// eslint-disable-next-line import/no-default-export
+export default defineConfig({
+    test: {
+        reporters: process.env.GITHUB_ACTIONS
+            ? ["default", new GithubActionsReporter()]
+            : "default",
+    },
+});
